dd951002858968f9d418a6569f3c3d8544764f74
piijey <pj.223f@gmail.com>
2024-04-16 21:56:44 +0900

    split try-kuromoji to kuromoji.js-react-app repo

--- try-kuromoji/config-overrides.js
+++
@@ -1,7 +0,0 @@
-module.exports = function override(config, env) {
-    config.resolve.fallback = {
-      ...config.resolve.fallback,
-      "path": require.resolve("path-browserify"),
-    };
-    return config;
-  };
