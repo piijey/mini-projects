5489cda7df097908033a5549aeb9cbef14eec2c0
piijey <pj.223f@gmail.com>
2024-10-07 16:21:21 +0900

    fixed & added test cases for eloquent-javascript exercise chapter 4-4

--- eloquent-javascript/e4_4_comparison.js
+++ eloquent-javascript/e4_4_comparison.js
@@ -1,20 +1,27 @@
+// 4. Data Structures: Objects and Arrays
+
 function deepEqual(objA, objB){
-    function recursiveEqual(oA, oB) {
-        for (k of Object.keys(oA)) {
-            if (oA[k] === oB[k]) {
-                return true;
-            } else if (oA[k] && oB[k] && typeof oA[k] == typeof oB[k] && typeof oA[k] === "object") {
-                return recursiveEqual(oA[k], oB[k]);
-            } else {
-                return false;
-            }
-        }
+    if (objA === objB) return true;
+    if (objA == null || objB == null || 
+        typeof objA != typeof objB || typeof objA != "object") return false;
+
+    if (Object.keys(objA).length != Object.keys(objB).length ) return false;
+    for (k of Object.keys(objA)) {
+        if (!Object.keys(objB).includes(k) ||
+            !deepEqual(objA[k], objB[k])) return false;
     }
-    return recursiveEqual(objA, objB);
+    return true;
 } 
 
 let obj = {here: {is: "an"}, object: 2};
 console.log(deepEqual(obj, obj));  // true
 console.log(deepEqual(obj, {here: 1, object: 2}));  // false
 console.log(deepEqual(obj, {here: {is: "an"}, object: 2}));  // true
+
+console.log("---");
 console.log(deepEqual(obj, {here: null, object: 2}));  // false
+console.log(deepEqual({a: 1, b: 2}, {a: 1}));  // false
+console.log(deepEqual({a: {b: 1, c: 2}}, {a: {b: 1}}));  // false
+console.log(deepEqual({a: null}, {a: null}));  // true
+console.log(deepEqual({}, {}));  // true
+console.log(deepEqual([1, 2, 3], [1, 2, 3]));  // true
