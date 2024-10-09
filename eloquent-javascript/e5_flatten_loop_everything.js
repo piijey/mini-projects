b34b17872eee1f28ad93f3d5b91d8c2ab702bd3d
piijey <pj.223f@gmail.com>
2024-10-09 22:12:01 +0900

    add eloquent-javascript exercise chapter 5-2,3

--- eloquent-javascript/e5_flatten_loop_everything.js
+++ eloquent-javascript/e5_flatten_loop_everything.js
@@ -4,3 +4,33 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 console.log(arrays.reduce((a, b) => a.concat(b), []));  // [ 1, 2, 3, 4, 5, 6 ]
 
+
+// Your own loop
+function loop(value, test, update, body) {
+    if (!test(value)) return;
+    body(value);
+    loop(update(value), test, update, body);
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+
+
+// Everything
+function every1(array, predicate) {
+    for (let element of array) {
+        if (!predicate(element)) return false;
+    }
+    return true;
+}
+
+console.log(every1([1, 3, 5], n => n < 10));  // true
+console.log(every1([2, 4, 16], n => n < 10));  // false
+console.log(every1([], n => n < 10));  // true
+
+function every2(array, predicate) {
+    return !array.some((element) => !predicate(element));
+}
+
+console.log(every2([1, 3, 5], n => n < 10));  // true
+console.log(every2([2, 4, 16], n => n < 10));  // false
+console.log(every2([], n => n < 10));  // true
